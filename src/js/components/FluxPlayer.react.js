af73200a624e9039ff4ebfb3319144296d41ca10
serzilo <designkota@mail.ru>
2015-10-03 16:17:31 +0300

    handled events on progress

--- src/js/components/FluxPlayer.react.js
+++ src/js/components/FluxPlayer.react.js
@@ -1,6 +1,45 @@
 var React = require('react');
 var $ = require('jquery');
 
+function getOffset(elem) {
+    if (elem.getBoundingClientRect) {
+        return getOffsetRect(elem);
+    } else {
+        return getOffsetSum(elem);
+    }
+}
+
+function getOffsetSum(elem) {
+    var top=0, 
+    	left=0;
+
+    while(elem) {
+        top = top + parseInt(elem.offsetTop);
+        left = left + parseInt(elem.offsetLeft);
+        elem = elem.offsetParent;
+    }
+
+    return {top: top, left: left};
+}
+
+function getOffsetRect(elem) {
+    var box = elem.getBoundingClientRect(),
+    	body = document.body,
+    	docElem = document.documentElement,
+    	scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop,
+    	scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft,
+    	clientTop = docElem.clientTop || body.clientTop || 0,
+    	clientLeft = docElem.clientLeft || body.clientLeft || 0,
+    	top  = box.top +  scrollTop - clientTop,
+    	left = box.left + scrollLeft - clientLeft;
+
+    return { top: Math.round(top), left: Math.round(left) };
+}
+
+function isTouchDevice() {
+	return !!('ontouchstart' in window);
+}
+
 var FluxPlayer = React.createClass({
 	getInitialState: function() {
 		return {
@@ -10,7 +49,7 @@ var FluxPlayer = React.createClass({
 				full: '3:15'
 			},
 			progress: {
-				width: '20%'
+				width: '0'
 			},
 			track: 'Muse - Starlight'
 		}
@@ -40,6 +79,77 @@ var FluxPlayer = React.createClass({
 		e.preventDefault();
 
 	},
+	progressOffsetLeft: 0,
+	progressWidth: 0,
+	startMoveHandler: function(e){
+		if ((isTouchDevice() == false && e.type == 'mousedown') || (isTouchDevice() == true && e.type == 'touchstart')){
+			e.preventDefault();
+
+			var progressHtml = this.refs.progress.getDOMNode(),
+				percents = 0,
+				_this = this,
+				handledEvents = {},
+				ns = '.progress';
+
+			if (isTouchDevice() == false){
+				handledEvents = {
+					move: 'mousemove' + ns,
+					end:  'mouseup' + ns
+				}
+			} else {
+				handledEvents = {
+					move: 'touchmove' + ns,
+					end:  'touchend' + ns
+				}
+
+				e = e.touches[0];
+			}
+
+			this.progressOffsetLeft = getOffset(progressHtml).left;
+			this.progressWidth = progressHtml.offsetWidth;
+
+			percents = this.moveHandlerCalculate(e);
+			this.setState({progress: {width: percents + '%' }});
+
+			$(window).on(handledEvents.move ,function(e){
+				e.preventDefault();
+
+				var percents = 0;
+
+				if (isTouchDevice() == true){
+					e = e.originalEvent.touches[0];
+				}
+
+				percents = _this.moveHandlerCalculate(e);
+				_this.setState({progress: {width: percents + '%' }});
+			}).on(handledEvents.end ,function(e){
+				e.preventDefault();
+
+				$(this).off('.progress');
+			});
+		}
+	},
+	moveHandlerCalculate: function(e){
+		return this.percentCalculate(e.clientX, this.progressWidth, this.progressOffsetLeft);
+	},
+	percentCalculate: function(clientX, width, offsetLeft){
+		var maxClientX = width + offsetLeft,
+			percents = 0;
+
+		if (clientX > maxClientX){
+			clientX = maxClientX;
+		} else if (clientX < 0){
+			clientX = 0;
+		}
+
+		percents = ((clientX - (offsetLeft ? offsetLeft : 0)) / width) * 100;
+
+		if (percents < 0){
+			percents = 0;
+		}
+
+		return percents;
+	},
 	render: function() {
 		return (
 			<div className="layout__footer">
@@ -49,7 +159,7 @@ var FluxPlayer = React.createClass({
 							{this.state.track}
 						</div>
 						<div className="player__tools">
-							<div className="player__progress">
+							<div ref="progress" className="player__progress" onMouseDown={this.startMoveHandler} onTouchStart={this.startMoveHandler}>
 								<div className="player__progress-bg" style={{width: this.state.progress.width}}></div>
 							</div>
 
